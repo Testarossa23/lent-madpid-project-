32fa544000340d3ffcd39cd4701a3c63791aec67
Testarossa23 <77401441+Testarossa23@users.noreply.github.com>
2021-02-06 18:16:37 +0100

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,36 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":return img`
+. . . . . 3 3 . . . . . . . . . 
+. . . . 3 3 3 3 . . . . . . . . 
+. . . . 3 3 3 3 . . . . . . . . 
+. . . . . 3 3 . . . . . . . . . 
+. . . . 1 f f 1 . . . . . . . . 
+. . . 1 f f f f 1 . . . . . . . 
+. . . 1 f f f f 1 . . . . . . . 
+. . . . 1 f f 1 . . . . . . . . 
+. . . f f 1 1 f f . . . . . . . 
+. . . f . . . . f . . . . . . . 
+. . . f . . . . f . . . . . . . 
+. . e e . . . . e e . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,1 +1,35 @@
-
+scene.onOverlapTile(SpriteKind.Player, assets.tile`tile4`, function (sprite, location) {
+    game.over(true)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mySprite.vy == 0) {
+        mySprite.vy = -125
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`tile0`, function (sprite, location) {
+    game.over(false, effects.melt)
+})
+let mySprite: Sprite = null
+scene.setBackgroundColor(9)
+mySprite = sprites.create(img`
+    . . . . . 3 3 . . . . . . . . . 
+    . . . . 3 3 3 3 . . . . . . . . 
+    . . . . 3 3 3 3 . . . . . . . . 
+    . . . . . 3 3 . . . . . . . . . 
+    . . . . 1 f f 1 . . . . . . . . 
+    . . . 1 f f f f 1 . . . . . . . 
+    . . . 1 f f f f 1 . . . . . . . 
+    . . . . 1 f f 1 . . . . . . . . 
+    . . . f f 1 1 f f . . . . . . . 
+    . . . f . . . . f . . . . . . . 
+    . . . f . . . . f . . . . . . . 
+    . . e e . . . . e e . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite, 100, 0)
+tiles.setTilemap(tilemap`level1`)
+mySprite.ay = 350
+scene.cameraFollowSprite(mySprite)
